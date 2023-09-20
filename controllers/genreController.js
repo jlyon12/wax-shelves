fa1243db418d03bb2a3bb2074bff8ec5c29d6b55
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-20 16:05:39 -0600

    handle POST genre_create

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require('../models/genre');
 const Record = require('../models/record');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
 	res.render('genre_form', {
@@ -9,9 +10,33 @@ exports.genre_create_get = asyncHandler(async (req, res, next) => {
 	});
 });
 
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED - POST on genre_create');
-});
+exports.genre_create_post = [
+	body('name', 'Genre name must be at least 3 characters long')
+		.trim()
+		.isLength({ minLength: 3 })
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		const genre = new Genre({ name: req.body.name });
+
+		if (!errors.isEmpty()) {
+			res.render('genre_form', {
+				title: 'Create Genre',
+				genre: genre,
+			});
+		} else {
+			const genreExists = await Genre.findOne({ name: req.body.name })
+				.collation({ locale: 'en', strength: 2 })
+				.exec();
+			if (genreExists) {
+				res.redirect(genreExists.url);
+			} else {
+				await genre.save();
+				res.redirect(genre.url);
+			}
+		}
+	}),
+];
 
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED - GET on genre_delete');

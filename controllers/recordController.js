adc8a09143e5c2d2b0579f11bc9122062ac6d0f5
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-20 19:50:43 -0600

    handle POST record_create

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -2,6 +2,7 @@ const Record = require('../models/record');
 const Genre = require('../models/genre');
 const Artist = require('../models/artist');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 exports.index = asyncHandler(async (req, res, next) => {
 	const [recordCount, artistCount, genreCount] = await Promise.all([
@@ -33,9 +34,57 @@ exports.record_create_get = asyncHandler(async (req, res, next) => {
 	});
 });
 
-exports.record_create_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED - POST on record_create');
-});
+exports.record_create_post = [
+	// Concert genre to an array
+	(req, res, next) => {
+		if (!(req.body.genre instanceof Array)) {
+			if (typeof req.body.genre === 'undefined') req.body.genre = [];
+			else req.body.genre = new Array(req.body.genre);
+		}
+		next();
+	},
+
+	body('title', 'Title can not be empty').trim().isLength({ min: 1 }).escape(),
+	body('artist', 'Artist can not be empty')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('label').trim().escape(),
+	body('cat_number').trim().escape(),
+	body('release_date').trim().escape(),
+	body('genre.*').escape(),
+
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+
+		const record = new Record({
+			title: req.body.title,
+			artist: req.body.artist,
+			label: req.body.label,
+			cat_number: req.body.cat_number,
+			release_date: req.body.release_date,
+			genre: req.body.genre,
+		});
+
+		if (!errors.isEmpty()) {
+			const [allArtists, allGenres] = await Promise.all([
+				Artist.find({}, 'name').sort({ name: 1 }).exec(),
+				Genre.find({}, 'name').sort({ name: 1 }).exec(),
+			]);
+
+			res.render('record_form', {
+				title: 'Create Record',
+				record: record,
+				artist_list: allArtists,
+				genre_list: allGenres,
+				errors: errors.array(),
+			});
+		} else {
+			await record.save();
+			res.redirect(record.url);
+		}
+	}),
+];
 
 exports.record_delete_get = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED - GET on record_delete');
